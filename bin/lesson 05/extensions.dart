ce611f4a61c26762eaaf919eb12b71f79aad678f
Anatoliy Zverev <azverinus@gmail.com>
2022-03-11 17:57:47 +0300

    Добавлены  материалы для 5-й лекции

---
+++ bin/lesson 05/extensions.dart
@@ -0,0 +1,12 @@
+/// 3.plus(4).minis(1) => 6
+
+// extension Math on int {
+//   int plus(int value) => this + value;
+
+//   int minus(int value) => this - value;
+// }
+
+void main() {
+  // final val = 3.plus(4).minus(1);
+  // print(val);
+}
