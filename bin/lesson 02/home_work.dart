851fa62497736abe0ae4a4d18f272ca4547826bb
TheBestOfGeeks <Mossd925@gmail.com>
2022-03-20 10:53:27 +0300

    home_work_lesson2
    Домашнее задание ко второму уроку Александр, ник в дискоре Sandro925.

--- bin/lesson 02/home_work.dart
+++ bin/lesson 02/home_work.dart
@@ -7,10 +7,25 @@
 /// нужно написать логику, которая будет возвращать строку с указанием числа и нужного склонения существительного
 /// IN: 3, ['день', 'дня', 'дней']
 /// OUT: '3 дня'
-
 String numeral(int count, List<String> variants) {
-  // ваш код
-  return "";
+  // преобразуем count в String так как он нам много где понадобится
+  String countToString = count.toString();
+  // вытаскиваем последнюю цифру count
+  var lastNum = int.tryParse(String.fromCharCode(countToString.runes.last));
+  // вытаскиваем две последние цифры count(если число имеет 3 или более цифры)
+  var twoNum = int.tryParse(
+      countToString.substring(count > 99 ? countToString.runes.length - 2 : 0));
+
+
+  if (lastNum == 0 || (twoNum! > 4 && twoNum < 20)) {
+    return '$count ${variants.last}';
+  }  else if (lastNum == 1) {
+    return '$count ${variants.first}';
+  } else if (lastNum! > 1 && lastNum < 5) {
+    return '$count ${variants[1]}';
+  } else {
+    return '$count ${variants.last}';
+  }
 }
 
 /// 2-я функция ago, принимает 1 аргумент
@@ -26,10 +41,21 @@ String numeral(int count, List<String> variants) {
 ///
 /// IN: DateTime.now().subtract(Duration(minutes: 10)
 /// OUT: '10 минут назад'
-
 String ago(DateTime date) {
-  // Ваш код
-  return "";
+  var time = DateTime.now().difference(date);
+  String pastOrFuture = date.isAfter(DateTime.now()) ? ' до указаной даты' : ' назад';
+  if(time.inMinutes < 60){
+    return  numeral(time.inMinutes, ['минута', 'минуты', 'минут']) + pastOrFuture;
+  } else if (time.inHours < 24) {
+    return numeral(time.inHours, ['час', 'часа', 'часов']) + pastOrFuture;
+  } else if (time.inDays < 7) {
+    return numeral(time.inDays, ['день', 'дня', 'дней']) + pastOrFuture;
+  } else if ((time.inDays ~/ 7) < 4 ) {
+    return numeral(time.inDays ~/ 7, ['неделя', 'недели', 'недель']) + pastOrFuture;
+  } else {
+    return numeral(time.inDays ~/ 30, ['месяц', 'месяца', 'месяцев']) + pastOrFuture;
+  }
+
 }
 
 void main(List<String> arguments) {
@@ -40,3 +66,4 @@ void main(List<String> arguments) {
   print(ago(DateTime.parse('2022-03-15 14:00:00')));
   print(ago(DateTime.parse('2022-03-20 19:00:00')));
 }
+
