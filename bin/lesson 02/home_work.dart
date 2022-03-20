13c3e539480b43906a1f5e5ce1d653c3a2c5c240
Sergei Tskhovrebov <sinxwal@gmail.com>
2022-03-20 23:11:16 +0300

    lesson 2 tasks complete

--- bin/lesson 02/home_work.dart
+++ bin/lesson 02/home_work.dart
@@ -8,9 +8,25 @@
 /// IN: 3, ['день', 'дня', 'дней']
 /// OUT: '3 дня'
 
+/*
+ * Russian has 3 plural forms:
+ * Form 0 for 1, 21, 31, 41, 51, 61...
+ * Form 1 for 2, 3, 4, 22, 23, 24...
+ * Form 2 for 0, 5, 6, 7, 8, 9...
+ * https://translations.launchpad.net/+languages/ru
+ * https://github.com/translate/l10n-guide/blob/master/docs/l10n/pluralforms.rst
+ */
 String numeral(int count, List<String> variants) {
-  // ваш код
-  return "";
+  if (variants.length < 3) {
+    throw Exception('Russian locale requires 3 variants');
+  }
+  if (count % 10 == 1 && count % 100 != 11) {
+    return variants[0];
+  } else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
+    return variants[1];
+  } else {
+    return variants[2];
+  }
 }
 
 /// 2-я функция ago, принимает 1 аргумент
@@ -18,7 +34,7 @@ String numeral(int count, List<String> variants) {
 ///
 /// нужно написать логику, которая будет возвращать строку с указанием сколько прошло
 /// (минут, часов, дней (до 7), недель (до 4) или месяцев) с переданной даты
-/// При этом не нужно выводить только самое большое измерение прошедшего времени
+/// При этом нужно выводить только самое большое измерение прошедшего времени
 /// Если события было более месяца назад, то только месяцы (3 месяца назад)
 /// Если событие совсем недавнее, то либо минуты, либо часы (5 минут назад или 6 часов назад)
 /// IN: DateTime.parse('2016-02-27 13:27:00')
@@ -27,9 +43,26 @@ String numeral(int count, List<String> variants) {
 /// IN: DateTime.now().subtract(Duration(minutes: 10)
 /// OUT: '10 минут назад'
 
+String formatter(int count, String plural) {
+  return count.toString() + " " + plural + " назад";
+}
+
 String ago(DateTime date) {
-  // Ваш код
-  return "";
+  final currentDate = DateTime.now();
+  final diff = currentDate.difference(date);
+  if (diff.inDays > 30) {
+    final monthsCount = diff.inDays ~/ 30;
+    return formatter(monthsCount, numeral(monthsCount, ['месяц', 'месяца', 'месяцев']));
+  } else if (diff.inDays > 7) {
+    final weeksCount = diff.inDays ~/ 7;
+    return formatter(weeksCount, numeral(weeksCount, ['неделя', 'недели', 'недель']));
+  } else if (diff.inDays > 1) {
+    return formatter(diff.inDays, numeral(diff.inDays, ['день', 'дня', 'дней']));
+  } else if (diff.inHours > 1) {
+    return formatter(diff.inHours, numeral(diff.inHours, ['час', 'часа', 'часов']));
+  } else {
+    return formatter(diff.inMinutes, numeral(diff.inMinutes, ['минута', 'минуты', 'минут']));
+  }
 }
 
 void main(List<String> arguments) {
