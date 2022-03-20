fb6c30b0a7328ad39ce17e07af8ba12dfc11a96b
chevic4 <78251945+chevic4@users.noreply.github.com>
2022-03-20 19:18:29 +0300

    Update home_work.dart

--- bin/lesson 02/home_work.dart
+++ bin/lesson 02/home_work.dart
@@ -7,10 +7,24 @@
 /// нужно написать логику, которая будет возвращать строку с указанием числа и нужного склонения существительного
 /// IN: 3, ['день', 'дня', 'дней']
 /// OUT: '3 дня'
-
 String numeral(int count, List<String> variants) {
-  // ваш код
-  return "";
+  String s = count.toString() + ' ';
+  if (count < 0) {
+    return 'количественное значение не должно быть отрицательным!';
+  }
+  switch (count % 10) {
+    case 1: // день 1
+      return s + variants[0];
+      break;
+    case 2:
+    case 3:
+    case 4: // дня 2 3 4
+      return s + variants[1];
+      break;
+    default: // дней 5 6 7 8 9 0
+      return s + variants[2];
+      break;
+  }
 }
 
 /// 2-я функция ago, принимает 1 аргумент
@@ -26,17 +40,50 @@ String numeral(int count, List<String> variants) {
 ///
 /// IN: DateTime.now().subtract(Duration(minutes: 10)
 /// OUT: '10 минут назад'
-
 String ago(DateTime date) {
-  // Ваш код
-  return "";
+  final date1 = DateTime.now();
+  if (date1.isBefore(date))
+    return 'дата не наступила!';
+  else {
+    int i = 0;
+    i = date1.year - date.year;
+    if (i > 0)
+      return numeral(i, ['год', 'года', 'лет']) + ' назад';
+    else {
+      i = date1.month - date.month;
+      if (i > 0)
+        return numeral(i, ['месяц', 'месяца', 'месяцев']) + ' назад';
+      else {
+        i = date1.day - date.day;
+        if (i > 0) {
+          if (i < 8)
+            return numeral(i, ['день', 'дня', 'дней']) + ' назад';
+          else
+            return numeral(i ~/ 7, ['нелеля', 'недели', 'недель']) + ' назад';
+        } else {
+          i = date1.hour - date.hour;
+          if (i > 0)
+            return numeral(i, ['час', 'часа', 'часов']) + ' назад';
+          else {
+            i = date1.minute - date.minute;
+            if (i > 0)
+              return numeral(i, ['минута', 'минуты', 'минут']) + ' назад';
+          }
+        }
+      }
+    }
+  }
+  return '';
 }
 
 void main(List<String> arguments) {
-  print(numeral(19, ['день', 'дня', 'дней']));
+  print(numeral(1, ['день', 'дня', 'дней']));
   print(numeral(5, ['неделя', 'недели', 'недель']));
 
   print(ago(DateTime.parse('2016-02-27 13:27:00')));
   print(ago(DateTime.parse('2022-03-15 14:00:00')));
   print(ago(DateTime.parse('2022-03-20 19:00:00')));
+
+  print(ago(DateTime.parse('2022-03-05 14:00:00')));
 }
+
