1a9a621edf71838821361fb96a391e482aedfc33
takahasi6661 <60356662+takahasi6661@users.noreply.github.com>
2022-03-21 20:10:41 +0300

    Update home_work.dart

--- bin/lesson 02/home_work.dart
+++ bin/lesson 02/home_work.dart
@@ -1,42 +1,62 @@
-/// Нобходимо написать реализацию двух функций
-///
-/// 1-я функция numeral, принимает два аргумента
-/// - число count — количество сущностей
-/// - List<String> варианты склонений (например: комментарий, комментария, комментариев)
-///
-/// нужно написать логику, которая будет возвращать строку с указанием числа и нужного склонения существительного
-/// IN: 3, ['день', 'дня', 'дней']
-/// OUT: '3 дня'
-
+//ник в discord: Alexandra_IL 
 String numeral(int count, List<String> variants) {
-  // ваш код
-  return "";
+  if (count < 0 || variants.length != 3) {
+    return 'Указано неверное кол-во дней ($count) или неверные варианты склонений: $variants';
+  } else {
+    final lastDigit = count % 10;
+    final twoLastDigits = count % 100;
+    var i = 0;
+    if (lastDigit == 1 && twoLastDigits != 11) {
+      i = 0;
+    } else if (lastDigit >= 2 &&
+        lastDigit <= 4 &&
+        !(twoLastDigits >= 12 && twoLastDigits <= 14)) {
+      i = 1;
+    } else {
+      i = 2;
+    }
+    return "$count ${variants[i]}";
+  }
 }
-
-/// 2-я функция ago, принимает 1 аргумент
-/// - DateTime date — метка даты и времени
-///
-/// нужно написать логику, которая будет возвращать строку с указанием сколько прошло
-/// (минут, часов, дней (до 7), недель (до 4) или месяцев) с переданной даты
-/// При этом не нужно выводить только самое большое измерение прошедшего времени
-/// Если события было более месяца назад, то только месяцы (3 месяца назад)
-/// Если событие совсем недавнее, то либо минуты, либо часы (5 минут назад или 6 часов назад)
-/// IN: DateTime.parse('2016-02-27 13:27:00')
-/// OUT: 'N месяцев назад'
-///
-/// IN: DateTime.now().subtract(Duration(minutes: 10)
-/// OUT: '10 минут назад'
 
 String ago(DateTime date) {
-  // Ваш код
-  return "";
+  var result = '';
+  const listOfTime = [
+    ['минута', 'минуты', 'минут'],
+    ['час', 'часа', 'часов'],
+    ['день', 'дня', 'дней'],
+    ['неделя', 'недели', 'недель'],
+    ['месяц', 'месяца', 'месяцев']
+  ];
+  final now = DateTime.now();
+  if (now.isBefore(date)) {
+    return 'Это дата из будущего';
+  } else {
+    final duration = now.difference(date).inMinutes;
+    if (duration < 60) {
+      result = numeral(duration, listOfTime[0]);
+    } else if (duration < 1440) {
+      result = numeral(duration ~/ 60, listOfTime[1]);
+    } else if (duration < 10080) {
+      result = numeral(duration~/1440, listOfTime[2]);
+    } else if (duration < 40320) {
+      result = numeral(duration ~/10080, listOfTime[3]);
+    } else {
+      result = numeral(duration~/40320, listOfTime[4]);
+    }
+    return '$result назад';
+  }
 }
 
-void main(List<String> arguments) {
+void main() {
   print(numeral(19, ['день', 'дня', 'дней']));
   print(numeral(5, ['неделя', 'недели', 'недель']));
+  print(numeral(-9, ['день', 'дня', 'дней']));
+  print(numeral(112, ['день', 'дня', 'дней']));
+  print(numeral(122, ['день', 'дня', 'дней']));
 
   print(ago(DateTime.parse('2016-02-27 13:27:00')));
   print(ago(DateTime.parse('2022-03-15 14:00:00')));
   print(ago(DateTime.parse('2022-03-20 19:00:00')));
+  print(ago(DateTime.parse('2022-03-23 11:43:00')));
 }
