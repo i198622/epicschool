ac75e804be65f314541e961bee947a81a3047cb6
Alexey M. Pereverzev <boss.pahan@gmail.com>
2022-03-23 17:20:38 +0300

    Добавление ДЗ по 3 уроку. Пирамида. Сумма положительных

--- bin/lesson 3/homework.dart
+++ bin/lesson 3/homework.dart
@@ -6,19 +6,22 @@
 // pyramid(2) => [ [1], [1, 1] ]
 // pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
 
-pyramid(int n) {
-  // ваш код
-}
+List<List<String>> pyramid(int n, {String fill = '1'}) =>
+    List.generate(n, (index) => List<String>.filled(index + 1, fill),
+        growable: false);
 
 // Напишите функцию, которая получает массив чисел и возвращаете сумму всех положительных.
 // Пример:
 // [1,-4,7,12] => 1 + 7 + 12 = 20
 
-sumOfPositive(List<int> nums) {
-  // Ваш код
-}
+int sumOfPositive(List<int> nums) => nums.fold(
+    0,
+    (previousValue, element) =>
+        element > 0 ? previousValue + element : previousValue);
 
 void main(List<String> arguments) {
-  pyramid(10);
-  sumOfPositive([10, -2, 30, 2]);
+  for (var i = 0; i != 4; i++) print(pyramid(i));
+  pyramid(10, fill: '').forEach(print);
+
+  print('The sum of all positive: ${sumOfPositive([10, -2, 0, 30, 2])}');
 }
