f05a0735d3840514317e50e0ea29f654cb3b9c81
takahasi6661 <60356662+takahasi6661@users.noreply.github.com>
2022-03-26 16:08:56 +0300

    Update homework.dart

--- bin/lesson 3/homework.dart
+++ bin/lesson 3/homework.dart
@@ -7,18 +7,30 @@
 // pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
 
 pyramid(int n) {
-  // ваш код
+ List <List<int>>list=[];
+  for (int i = 1; i <= n; i++){
+    final List<int>listInner=List.filled(i,1);
+    list.add(listInner);
 }
+  return list;
+}
+
 
 // Напишите функцию, которая получает массив чисел и возвращаете сумму всех положительных.
 // Пример:
 // [1,-4,7,12] => 1 + 7 + 12 = 20
 
 sumOfPositive(List<int> nums) {
-  // Ваш код
+  var summa=0;
+  nums.forEach((element){
+    if (element>0){
+      summa+=element;
+    }
+  });
+  return summa;
 }
 
 void main(List<String> arguments) {
-  pyramid(10);
-  sumOfPositive([10, -2, 30, 2]);
+  print(pyramid(4));
+  print(sumOfPositive([10, -2, 30, 2]));
 }
