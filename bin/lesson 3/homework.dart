40c5bd47d6cf776963dd87057824d997428a5c81
padmelina <elizaveta.sukhanova@rt.ru>
2022-03-27 20:52:03 +0300

    Домашняя работа к лекции номер 2. Суханова Елизавета.

--- bin/lesson 3/homework.dart
+++ bin/lesson 3/homework.dart
@@ -6,19 +6,23 @@
 // pyramid(2) => [ [1], [1, 1] ]
 // pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
 
-pyramid(int n) {
-  // ваш код
+List<List<int>> pyramid(int n) {
+  final List<List<int>> result = [];
+  for (var i = 1; i < n + 1; i++) {
+    result.add(List.generate(i, (index) => 1));
+  }
+  return result;
 }
 
 // Напишите функцию, которая получает массив чисел и возвращаете сумму всех положительных.
 // Пример:
 // [1,-4,7,12] => 1 + 7 + 12 = 20
 
-sumOfPositive(List<int> nums) {
-  // Ваш код
+int sumOfPositive(List<int> nums) {
+  return nums.where((element) => !element.isNegative).reduce((value, element) => value + element);
 }
 
 void main(List<String> arguments) {
-  pyramid(10);
-  sumOfPositive([10, -2, 30, 2]);
+  print(pyramid(10));
+  print(sumOfPositive([10, -2, 30, 2]));
 }
