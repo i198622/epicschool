b4f04a3e31eb5e75706a32627c29b991872f95b7
padmelina <elizaveta.sukhanova@rt.ru>
2022-03-31 01:03:25 +0300

    Домашняя работа к лекции номер 4. Суханова Елизавета.

--- bin/lesson 4/home_work.dart
+++ bin/lesson 4/home_work.dart
@@ -3,6 +3,27 @@
 // 3. Добавьте в класс метод, getter который красиво форматирует Student для печати полей firstName и lastName (должны возвращать с заглавной буквы)
 // 4. Переопределите в классе Student метод toString для печати класса
 
+class Student {
+  final String firstName;
+  final String lastName;
+
+  const Student(this.firstName, this.lastName);
+  Student.named({ required this.firstName, required this.lastName });
+
+  String get fullName =>
+      '${_getFromUpperCase(firstName)} ${_getFromUpperCase(lastName)}';
+
+
+  @override
+  String toString() {
+    return fullName;
+  }
+
+  String _getFromUpperCase(String str) => '${str[0].toUpperCase()}${str.substring(1)}';
+}
+
 void main(List<String> arguments) {
-  
+  Student student1 = Student('ivan', 'ivanov');
+  print(student1.fullName);
+  print(student1);
 }
