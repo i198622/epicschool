2dc24507394d44f246a2ccd96fa57d788eb38778
Sergei Tskhovrebov <sinxwal@gmail.com>
2022-04-01 20:30:51 +0300

    hw lesson 04 fixed

--- bin/lesson 4/home_work.dart
+++ bin/lesson 4/home_work.dart
@@ -3,6 +3,28 @@
 // 3. Добавьте в класс метод, getter который красиво форматирует Student для печати полей firstName и lastName (должны возвращать с заглавной буквы)
 // 4. Переопределите в классе Student метод toString для печати класса
 
+String capitalize(String string) {
+  if (string.isEmpty) {
+    return string;
+  }
+  return string[0].toUpperCase() + string.substring(1);
+}
+
+class Student {
+  final String firstName;
+  final String lastName;
+
+  const Student({required this.firstName, required this.lastName});
+
+  String get fullName => '${capitalize(firstName)} ${capitalize(lastName)}';
+
+  @override
+  String toString() {
+    return fullName;
+  }
+}
+
 void main(List<String> arguments) {
-  
+  final student = Student(firstName: 'alex', lastName: 'ivanov');
+  print(student);
 }
